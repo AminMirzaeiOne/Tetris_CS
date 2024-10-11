f4e1421d333358f09d9f3cc86540df42858dba6a
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-11 21:24:20 +0330

    - Create levelScale Filed In MatinWindow CS

--- Tetris/MainWindow.xaml.cs
+++ Tetris/MainWindow.xaml.cs
@@ -45,6 +45,7 @@ namespace Tetris
         private bool rightCollided = false;
         private bool isGameOver = false;
         private int gameSpeed;
+        private int levelScale = 60;// every 60 second increase level by 1 until 10
 
 
 
